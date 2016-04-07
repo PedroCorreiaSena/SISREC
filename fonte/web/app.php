10a49fbd5f4ef62187a8464bd1e1334fc94329c4
clagomess <cla.gomess@gmail.com>
2016-04-07 14:19:46 -0300

    Mudando para prod

--- fonte/web/app.php
+++ fonte/web/app.php
@@ -16,7 +16,7 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('dev', false);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
 
