2166d3524745060d53c7c82e1637fc2cf6d93ed7
claudio gomes <cla.gomess@gmail.com>
2016-06-15 20:06:55 -0300

    Fix #20

--- fonte/src/AdminBundle/Form/UsuarioType.php
+++ fonte/src/AdminBundle/Form/UsuarioType.php
@@ -119,6 +119,43 @@ class UsuarioType extends AbstractType
             )
         ));
 
+        $builder->add('uf', 'choice', array(
+            'label' => 'UF',
+            'attr' => array(
+                'class' => 'form-control'
+            ),
+            'choices' => array(
+                'AC' => 'Acre',
+                'AL' => 'Alagoas',
+                'AP' => 'Amapá',
+                'AM' => 'Amazonas',
+                'BA' => 'Bahia',
+                'CE' => 'Ceará',
+                'DF' => 'Distrito Federal',
+                'ES' => 'Espírito Santo',
+                'GO' => 'Goiás',
+                'MA' => 'Maranhão',
+                'MT' => 'Mato Grosso',
+                'MS' => 'Mato Grosso do Sul',
+                'MG' => 'Minas Gerais',
+                'PA' => 'Pará',
+                'PB' => 'Paraíba',
+                'PR' => 'Paraná',
+                'PE' => 'Pernambuco',
+                'PI' => 'Piauí',
+                'RJ' => 'Rio de Janeiro',
+                'RN' => 'Rio Grande do Norte',
+                'RS' => 'Rio Grande do Sul',
+                'RO' => 'Rondônia',
+                'RR' => 'Roraima',
+                'SC' => 'Santa Catarina',
+                'SP' => 'São Paulo',
+                'SE' => 'Sergipe',
+                'TO' => 'Tocantins'
+            )
+        ));
+
+
         $builder->add('cidade', 'text', array(
             'label' => 'Cidade',
             'attr' => array(
